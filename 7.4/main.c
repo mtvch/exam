e0cf4b475100a73803dedc1381403cb11d9e5a03
mtvch <67135906+mtvch@users.noreply.github.com>
2020-06-21 20:28:34 +0700

    Create main.c

---
+++ 7.4/main.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNIGNS
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct Time Time;
+
+struct Time {
+	char h;
+	char min;
+	char sec;
+};
+
+int convert_sec(Time* time) {
+	return (int)time->h * 3600 + (int)time->min * 60 + time->sec;
+}
+
+int add_time(Time* time, int add) {
+	int sec = convert_sec(time);
+	sec += add;
+	time->h = (sec / 3600) % 24;
+	time->min = (sec / 60 ) % 60;
+	time->sec = sec % 60;
+}
+
+int main() {
+	Time* time = malloc(sizeof(Time));
+	time->h = 15;
+	time->min = 30;
+	time->sec = 23;
+	add_time(time, 3600);
+	return 0;
+}
